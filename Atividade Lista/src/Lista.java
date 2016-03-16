2de2e44dd14e21cfce2f8c92975cbbe7d052109d
felipebastosb <felipe.bastos@ymail.com>
2016-03-16 15:54:05 -0300

    Pronto

--- Atividade Lista/src/Lista.java
+++ Atividade Lista/src/Lista.java
@@ -46,22 +46,41 @@ public class Lista
     
     public void Remover (int Valor)
     {
-        
+        NoLista Atual = this.inicio;
+        NoLista Proximo = Atual.proximo;
+            while(posAtual.valor!=valor) {
+                Atual = Atual.proximo;
+                Proximo = Proximo.proximo;
+            }
+            Atual.proximo = Proximo.proximo;   
     }
     
     public boolean Vazia ()
     {
-       return false; 
+        if(this.inicio==null)
+            return true;
+        else
+            return false; 
     }
     
     public int getValor(int Posicao)
     {
-        return 33;
+        NoLista Atual = this.inicio;
+        for(int x = 0; x<(pos-1);x++)
+            Atual = Atual.proximo;
+        return Atual.valor;
     }
     
-    public int getPosicao(int Vallor)
+    public int getPosicao(int Valor)
     {
-        return 33;
+        NoLista Atual = this.inicio;
+        int Posicao = 1;
+        while(Atual.valor!=valor)
+        {
+            Atual = Atual.proximo;
+            Posicao++;
+        }
+        return Posicao;
     }
     
     
