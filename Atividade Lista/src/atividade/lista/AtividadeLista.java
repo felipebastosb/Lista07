25aa0903fa6888a6323528eb8d8fc7052efc6519
felipebastosb <felipe.bastos@ymail.com>
2016-03-16 16:00:00 -0300

    Update AtividadeLista.java

--- Atividade Lista/src/atividade/lista/AtividadeLista.java
+++ Atividade Lista/src/atividade/lista/AtividadeLista.java
@@ -1,21 +1,23 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package atividade.lista;
 
-/**
- *
- * @author FACOMP
- */
 public class AtividadeLista {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+
+    public static void main(String[] args) 
+    {
+        Lista Teste = new Lista(); 
+        Teste.inserirInicio(4);
+        Teste.inserirFim(3);
+        Teste.inserirMeio(2);
+        Teste.inserirFim(1);
+        Teste.Remover(2);
+        Teste.getValor(1);
+        Teste.getPosicao(2);
+        Teste.Vazia();
+        System.out.println(Teste.getValor(1));
+        System.out.println(Teste.getPosicao(2));
+        System.out.println(Teste.Vazia(););
     }
     
 }
